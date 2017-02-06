098b4cfd527a8a2c61cf6be27c9287717e6dc0b8
Spencer Hakim <spencer@treble.fm>
2017-02-06 11:02:31 -0500

    Support .nvmrc

--- installNode.sh
+++ installNode.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-if [ -z "$NODE_VERSION" ]; then
+if [ ! -f ".nvmrc" ] && [ -z "$NODE_VERSION" ]; then
   echo "NODE_VERSION is undefined"
   exit 1
 fi
 
 nvm install $NODE_VERSION
-nvm alias default $NODE_VERSION
-nvm use default
+nvm use $NODE_VERSION
+nvm alias default `nvm current`
 nvm cache clear
 
 npm -g install npm
