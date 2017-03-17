bafb81ad724f26a46bee1bc14d082dbe0bf660ff
Spencer Hakim <spencer@treble.fm>
2017-03-17 15:25:08 -0500

    Added EC2_HOSTNAME env var

--- sh.sh
+++ sh.sh
@@ -1,5 +1,8 @@
 #!/bin/dash
 
+# NOTE - This allows containers to easily communicate with agent services running on the same EC2 instance
+export EC2_HOSTNAME=$(curl -s http://169.254.169.254/latest/meta-data/local-hostname)
+
 if [ "$PPID" = "0" ]; then
     # Use bash login shell for Docker RUN/CMD/ENTRYPOINT
     exec "/bin/bash" "-l" "$@"
