beba90d8dba072f79faa49b2b870df4a1f8546c6
Spencer Hakim <spencer@treble.fm>
2017-03-27 14:23:24 -0400

    “Why is this build process so slow on my laptop but fast on AWS…”

--- sh.sh
+++ sh.sh
@@ -1,7 +1,10 @@
 #!/bin/dash
 
-# NOTE - This allows containers to easily communicate with agent services running on the same EC2 instance
-export EC2_HOSTNAME=$(curl -s http://169.254.169.254/latest/meta-data/local-hostname)
+# Only call this if running on a Convox rack and EC2_HOSTNAME isn't already defined
+if [ -v "$RACK" ] && [ -z "$EC2_HOSTNAME" ]; then
+    # NOTE - This allows containers to easily communicate with agent services running on the same EC2 instance
+    export EC2_HOSTNAME=$(curl -s http://169.254.169.254/latest/meta-data/local-hostname)
+fi
 
 if [ "$PPID" = "0" ]; then
     # Use bash login shell for Docker RUN/CMD/ENTRYPOINT
