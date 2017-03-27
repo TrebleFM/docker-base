a11b8e1f5d860010595b8f8709c35df7f33ea776
Spencer Hakim <spencer@treble.fm>
2017-03-27 14:33:53 -0400

    Shell scripting fucking sucks

--- sh.sh
+++ sh.sh
@@ -1,7 +1,7 @@
 #!/bin/dash
 
 # Only call this if running on a Convox rack and EC2_HOSTNAME isn't already defined
-if [ -v "$RACK" ] && [ -z "$EC2_HOSTNAME" ]; then
+if [ "$RACK" != "" ] && [ "$EC2_HOSTNAME" = "" ]; then
     # NOTE - This allows containers to easily communicate with agent services running on the same EC2 instance
     export EC2_HOSTNAME=$(curl -s http://169.254.169.254/latest/meta-data/local-hostname)
 fi
