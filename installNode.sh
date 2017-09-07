765be0b145994c83e093009147bb23a30890346b
Spencer Hakim <spencer@treble.fm>
2017-09-07 12:47:04 -0400

    Yay, npm is fixed

--- installNode.sh
+++ installNode.sh
@@ -5,12 +5,7 @@ if [ ! -f ".nvmrc" ] && [ -z "$NODE_VERSION" ]; then
     exit 1
 fi
 
-nvm install $NODE_VERSION
+nvm install $NODE_VERSION --latest-npm
 nvm use $NODE_VERSION
 nvm alias default `nvm current`
 nvm cache clear
-
-# TODO - npm@5.4.0 is broken, uncomment this once fixed
-# https://github.com/npm/npm/issues/18324
-# npm -g install npm
-# npm -g upgrade
