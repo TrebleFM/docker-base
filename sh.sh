d6b6a62154f2eac119ab101dae4f60a54bc8f7cb
Spencer Hakim <spencer@treble.fm>
2017-09-28 13:53:07 -0400

    Limit curling EC2 metadata to 1 second (should be near-instantaneous in most cases)

--- sh.sh
+++ sh.sh
@@ -3,7 +3,7 @@
 # Only call this if running on a Convox rack and EC2_HOSTNAME isn't already defined
 if [ "$RACK" != "" ] && [ "$EC2_HOSTNAME" = "" ]; then
     # NOTE - This allows containers to easily communicate with agent services running on the same EC2 instance
-    export EC2_HOSTNAME=$(curl -s http://169.254.169.254/latest/meta-data/local-hostname)
+    export EC2_HOSTNAME=$(curl -sm 1 http://169.254.169.254/latest/meta-data/local-hostname)
 fi
 
 # Load NVM
